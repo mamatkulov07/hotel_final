1bc6223e9e19d339556978d1dd74657affbdb31c
Usman <usmanmamatkulov4.com>
2024-06-09 15:35:38 +0600

    k

--- hotel_site/hotel_site/settings.py
+++ hotel_site/hotel_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -174,3 +174,5 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_FORMS = {'signup': 'mysite.forms.CustomSignupForm'}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
